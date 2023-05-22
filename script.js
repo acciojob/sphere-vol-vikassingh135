d3cd9979cd19731598ae130d682a35b7b4168c92
Vikas Singh <69408149+vikassingh135@users.noreply.github.com>
2023-05-22 12:29:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function volume_sphere() {
     //Write your code here
 	let radius = parseInt(document.getElementById("radius").value)|| 0;
 	let volume = document.getElementById("volume");
-	volume.value = (4/3)*(22/7)*(radius*radius*radius);
+	return (4/3)*(22/7)*(radius*radius*radius);
   
 } 
 
